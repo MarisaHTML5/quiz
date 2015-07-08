876b752a400178ca03554954f18c4c9fc89b140a
MarisaHTML5 <mar.gonlez@gmail.com>
2015-07-08 13:48:09 +0200

    PreguntaEJScon estilosy demás

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -5,11 +5,33 @@ exports.question=function(req, res){
 
 };
 
+function removeAccents(word){
+    word = word.replace(/[ÀÁÂÄ]/g,"A");
+    word = word.replace(/[àáâä]/g,"a");
+    word = word.replace(/[ÈÉÊË]/g,"E");
+    word = word.replace(/[èéêë]/g,"e");
+    word = word.replace(/[ÌÍÎÏ]/g,"I");
+    word = word.replace(/[ìíîï]/g,"i");
+    word = word.replace(/[ÒÓÔÖ]/g,"O");
+    word = word.replace(/[òóôö]/g,"o");
+    word = word.replace(/[ÙÚÛÜ]/g,"U");
+    word = word.replace(/[ùúûü]/g,"u");
+    return word;
+}
+
+
+
 //GET/quizes/answer
 exports.answer = function (req, res){
 	if(req.query.respuesta==='Roma'){
+		if(removeAccents(req.query.respuesta).toUpperCase() === removeAccents('Roma').toUpperCase()){
 		res.render('quizes/answer', {respuesta:'Correcto'});
 	} else{
 		res.render('quizes/answer', {respuesta:'Incorrecto'});
 	}
 };
+
+
+
+
+
