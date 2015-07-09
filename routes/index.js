e9a77007cc56bc6f9cc59c6063f4d14ed1b0c6e5
MarisaHTML5 <mar.gonlez@gmail.com>
2015-07-09 09:44:23 +0200

    fin author

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var quizController= require ('../controllers/quiz_controller');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quizz' });
 });
-router.get('/quizes/author'); //esto de aquí no lo tengo claro, creo que hay algo más que añadir
+router.get('/quizes/author', quizController.author); //esto de aquí no lo tengo claro, creo que hay algo más que añadir
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
